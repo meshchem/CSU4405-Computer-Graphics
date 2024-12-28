0d629b369f1622d95377d8c5912ce66595a25e33
meshchem <meshchem@tcd.ie>
2024-12-28 16:04:39 +0100

    Update BasePlane.cpp
    added skybox struct

--- BasePlane.cpp
+++ BasePlane.cpp
@@ -67,6 +67,266 @@ static GLuint LoadTexture(const char *texture_file_path) {
 // Global variables
 GLuint tileProgramID;
 GLuint buildingProgramID;
+GLuint skyboxProgramID;
+
+struct Skybox {
+	glm::vec3 position;		// Position of the box 
+	glm::vec3 scale;		// Size of the box in each axis
+	
+	GLfloat vertex_buffer_data[72] = {	// Vertex definition for a canonical box
+		// Front face
+		-1.0f, -1.0f, 1.0f, 
+		1.0f, -1.0f, 1.0f, 
+		1.0f, 1.0f, 1.0f, 
+		-1.0f, 1.0f, 1.0f, 
+		
+		// Back face 
+		1.0f, -1.0f, -1.0f, 
+		-1.0f, -1.0f, -1.0f, 
+		-1.0f, 1.0f, -1.0f, 
+		1.0f, 1.0f, -1.0f,
+		
+		// Left face
+		-1.0f, -1.0f, -1.0f, 
+		-1.0f, -1.0f, 1.0f, 
+		-1.0f, 1.0f, 1.0f, 
+		-1.0f, 1.0f, -1.0f, 
+
+		// Right face 
+		1.0f, -1.0f, 1.0f, 
+		1.0f, -1.0f, -1.0f, 
+		1.0f, 1.0f, -1.0f, 
+		1.0f, 1.0f, 1.0f,
+
+		// Top face
+		-1.0f, 1.0f, 1.0f, 
+		1.0f, 1.0f, 1.0f, 
+		1.0f, 1.0f, -1.0f, 
+		-1.0f, 1.0f, -1.0f, 
+
+		// Bottom face
+		-1.0f, -1.0f, -1.0f, 
+		1.0f, -1.0f, -1.0f, 
+		1.0f, -1.0f, 1.0f, 
+		-1.0f, -1.0f, 1.0f, 
+	};
+
+	GLfloat color_buffer_data[72] = {
+		// Front, red
+		1.0f, 0.0f, 0.0f,
+		1.0f, 0.0f, 0.0f,
+		1.0f, 0.0f, 0.0f,
+		1.0f, 0.0f, 0.0f,
+
+		// Back, yellow
+		1.0f, 1.0f, 0.0f,
+		1.0f, 1.0f, 0.0f,
+		1.0f, 1.0f, 0.0f,
+		1.0f, 1.0f, 0.0f,
+
+		// Left, green
+		0.0f, 1.0f, 0.0f, 
+		0.0f, 1.0f, 0.0f,
+		0.0f, 1.0f, 0.0f,
+		0.0f, 1.0f, 0.0f,
+
+		// Right, cyan
+		0.0f, 1.0f, 1.0f, 
+		0.0f, 1.0f, 1.0f, 
+		0.0f, 1.0f, 1.0f, 
+		0.0f, 1.0f, 1.0f, 
+
+		// Top, blue
+		0.0f, 0.0f, 1.0f, 
+		0.0f, 0.0f, 1.0f,
+		0.0f, 0.0f, 1.0f,
+		0.0f, 0.0f, 1.0f,
+
+		// Bottom, magenta
+		1.0f, 0.0f, 1.0f,
+		1.0f, 0.0f, 1.0f, 
+		1.0f, 0.0f, 1.0f, 
+		1.0f, 0.0f, 1.0f,  
+	};
+
+    
+	// Modify the cube buffers so that the inner surfaces of the cube becomes front facing in counter-clockwise order.
+	// to position viewer inside the cube need to render inner faces
+    // reverse winding order, front face of each triangle is on inside of cube 
+	GLuint index_buffer_data[36] = {		
+        0, 3, 2,
+		0, 2, 1,
+
+		4, 7, 6,
+		4, 6, 5,
+
+		8, 11, 10,
+		8, 10, 9,
+
+		12, 15, 14,
+		12, 14, 13,
+
+		16, 19, 18,
+		16, 18, 17,
+
+		20, 23, 22,
+		20, 22, 21,
+ 
+	};
+
+	GLfloat uv_buffer_data[48] = {
+		// Pos Z
+		0.5f, 0.666f,  	// Top right
+		0.25f, 0.666f, 	// Top left
+		0.25f, 0.333f, 	// Bottom left
+		0.5f, 0.333f,  	// Bottom right
+
+		// Neg Z
+		1.0f, 0.666f,  	// Top right
+		0.75f, 0.666f, 	// Top left
+		0.75f, 0.333f, 	// Bottom left
+		1.0f, 0.333f,  	// Bottom right
+
+		// Pos X
+		0.75f, 0.666f, 	// Top right
+		0.5f, 0.666f,  	// Top left
+		0.5f, 0.333f,  	// Bottom left
+		0.75f, 0.333f, 	// Bottom right
+
+		// Neg X
+		0.25f, 0.666f, 	// Top right
+		0.0f, 0.666f,  	// Top left
+		0.0f, 0.333f,  	// Bottom left
+		0.25f, 0.333f, 	// Bottom right
+
+		// Neg Y
+		0.5f, 0.333f,  	// Top right
+		0.25f, 0.333f,  // Top left
+		0.25f, 0.0f,   	// Bottom left
+		0.5f, 0.0f,    	// Bottom right
+
+        // Pos Y
+		0.5f, 1.0f,    	// Top right
+		0.25f, 1.0f,   	// Top left
+		0.25f, 0.666f, 	// Bottom left
+		0.5f, 0.666f  	// Bottom right
+
+	};
+    
+	// OpenGL buffers
+	GLuint vertexArrayID; 
+	GLuint vertexBufferID; 
+	GLuint indexBufferID; 
+	GLuint colorBufferID;
+	GLuint uvBufferID;
+	GLuint textureID;
+
+	// Shader variable IDs
+	GLuint mvpMatrixID;
+	GLuint textureSamplerID;
+	
+
+	void initialize(glm::vec3 position, glm::vec3 scale) {
+		// Define scale of the skybox geometry
+		this->position = position;
+		this->scale = scale;
+
+		// Create a vertex array object
+		glGenVertexArrays(1, &vertexArrayID);
+		glBindVertexArray(vertexArrayID);
+
+		// Create a vertex buffer object to store the vertex data		
+		glGenBuffers(1, &vertexBufferID);
+		glBindBuffer(GL_ARRAY_BUFFER, vertexBufferID);
+		glBufferData(GL_ARRAY_BUFFER, sizeof(vertex_buffer_data), vertex_buffer_data, GL_STATIC_DRAW);
+
+		// Create a vertex buffer object to store the color data
+        for (int i = 0; i < 72; ++i) {
+        	color_buffer_data[i] = 1.0f; 
+    	}
+		glGenBuffers(1, &colorBufferID);
+		glBindBuffer(GL_ARRAY_BUFFER, colorBufferID);		
+		glBufferData(GL_ARRAY_BUFFER, sizeof(color_buffer_data), color_buffer_data, GL_STATIC_DRAW);
+
+
+		// create a uv buffer object to store the uv data 
+		glGenBuffers(1, &uvBufferID);
+		glBindBuffer(GL_ARRAY_BUFFER, uvBufferID);
+		glBufferData(GL_ARRAY_BUFFER, sizeof(uv_buffer_data), uv_buffer_data, GL_STATIC_DRAW);
+
+		// Create an index buffer object to store the index data that defines triangle faces
+		glGenBuffers(1, &indexBufferID);
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indexBufferID);
+		glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(index_buffer_data), index_buffer_data, GL_STATIC_DRAW);
+
+		// Create and compile our GLSL program from the shaders
+		skyboxProgramID = LoadShadersFromFile("../FinalProject/skybox.vert", "../FinalProject/skybox.frag");
+		if (skyboxProgramID == 0)
+		{
+			std::cerr << "Failed to load shaders." << std::endl;
+		}
+
+		// Get a handle for our "MVP" uniform
+		mvpMatrixID = glGetUniformLocation(skyboxProgramID, "MVP");
+		textureID = LoadTexture("../FinalProject/sky.png");
+		textureSamplerID = glGetUniformLocation(skyboxProgramID,"textureSampler");
+	}
+
+	void render(glm::mat4 cameraMatrix) {
+		glUseProgram(skyboxProgramID);
+
+		glEnableVertexAttribArray(0);
+		glBindBuffer(GL_ARRAY_BUFFER, vertexBufferID);
+		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+
+		glEnableVertexAttribArray(1);
+		glBindBuffer(GL_ARRAY_BUFFER, colorBufferID);
+		glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, 0);
+
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indexBufferID);
+
+		// Model transform 
+
+    	glm::mat4 modelMatrix = glm::mat4();
+    	modelMatrix = glm::scale(modelMatrix, scale);
+
+		// Set model-view-projection matrix
+		glm::mat4 mvp = cameraMatrix * modelMatrix;
+		glUniformMatrix4fv(mvpMatrixID, 1, GL_FALSE, &mvp[0][0]);  //sends mvp matrix to vertex shader
+
+		// Enable UV buffer and texture sampler
+		glEnableVertexAttribArray(2);
+ 		glBindBuffer(GL_ARRAY_BUFFER, uvBufferID);
+ 		glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 0, 0);
+
+		 // Set textureSampler to use texture unit 0
+		glActiveTexture(GL_TEXTURE0);
+ 		glBindTexture(GL_TEXTURE_2D, textureID);
+ 		glUniform1i(textureSamplerID, 0); 
+	
+		// Draw the box
+		glDrawElements(
+			GL_TRIANGLES,      // mode
+			36,    			   // number of indices
+			GL_UNSIGNED_INT,   // type
+			(void*)0           // element array buffer offset
+		);
+
+		glDisableVertexAttribArray(0);
+		glDisableVertexAttribArray(1);
+        //glDisableVertexAttribArray(2);
+	}
+
+	void cleanup() {
+		glDeleteBuffers(1, &vertexBufferID);
+		glDeleteBuffers(1, &colorBufferID);
+		glDeleteBuffers(1, &indexBufferID);
+		glDeleteVertexArrays(1, &vertexArrayID);
+		glDeleteBuffers(1, &uvBufferID);
+		glDeleteTextures(1, &textureID);
+		glDeleteProgram(skyboxProgramID);
+	}
+};
 
 struct Tile {
     float tileVertices[20] = {
@@ -461,6 +721,7 @@ struct Building {
 std::vector<Tile> tiles;
 std::vector<Building> buildings;
 std::vector<std::string> BuildingFacades;
+Skybox skybox; // Skybox instance
 
 void generateTiles(glm::vec3 position) {
     int centerX = static_cast<int>(std::floor(position.x / cellSize));
@@ -552,6 +813,8 @@ int main() {
     glClearColor(0.05f, 0.05f, 0.2f, 1.0f);
     glEnable(GL_DEPTH_TEST);
 
+    //skybox.initialize(glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(500.0f, 500.0f, 500.0f)); // Adjust scale if needed
+
     generateTiles(cameraPos);
     generateBuildings(cameraPos);
     srand(static_cast<unsigned int>(time(nullptr)));
@@ -566,13 +829,19 @@ int main() {
         generateBuildings(cameraPos);
 
         // Calculate view-projection matrix
-        glm::mat4 view = glm::lookAt(cameraPos, cameraPos + cameraFront, cameraUp);
+       glm::mat4 view = glm::lookAt(cameraPos, cameraPos + cameraFront, cameraUp);
         glm::mat4 projection = glm::perspective(glm::radians(45.0f), (float)windowWidth / windowHeight, 0.1f, 100.0f);
         glm::mat4 vp = projection * view;
 
         // Clear the screen and enable depth testing
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
         glEnable(GL_DEPTH_TEST);
+
+        // Render the Skybox
+        glm::mat4 skyboxViewMatrix = glm::mat4(glm::mat3(glm::lookAt(cameraPos, cameraPos + cameraFront, cameraUp)));
+        // Remove translation from view matrix for skybox rendering
+        glm::mat4 skyboxProjection = glm::perspective(glm::radians(45.0f), (float)windowWidth / windowHeight, 0.1f, 1000.0f);
+        skybox.render(skyboxProjection * skyboxViewMatrix);
 
         // Render Tiles
         glUseProgram(tileProgramID);
@@ -660,9 +929,14 @@ void handleCameraMovement(GLFWwindow* window) {
 
     if (glm::length(direction) > 0.0f)
         cameraPos += glm::normalize(direction) * cameraSpeed;
+    
+    // Update Skybox position to follow the camera
+    //skybox.position = cameraPos;
 }
 
 void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods) {
     if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
         glfwSetWindowShouldClose(window, GL_TRUE);
 }
+
+
